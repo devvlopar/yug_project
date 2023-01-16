a1cd1dc5a956634b0ba9557d1af2fbb72da52add
devvlopar <devangsingh34@gmail.com>
2023-01-16 16:14:05 +0530

    register completed

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,4 +138,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'beherapapukumar2@gmail.com'
-EMAIL_HOST_PASSWORD = 'ivweodivpyapxcag'
+EMAIL_HOST_PASSWORD = ''
